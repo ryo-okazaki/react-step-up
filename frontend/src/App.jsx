7eeff1deecc306b124bfbfbe6e5978264d6f78bd
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-25 23:52:42 +0900

    子コンポーネント作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import {ChildArea} from "./components/ChildArea.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  }
+  const onchangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onchangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
     </div>
   )
 }
