31bcdab5bfa98c7f32c73a31dee9644e5cc1d3c4
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-25 23:39:20 +0900

    React動作復習

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>{count}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
     </div>
   )
 }
