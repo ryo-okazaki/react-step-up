99639cea0a8f28417b51a46fce2f00aa0656407a
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-26 15:01:48 +0900

    ReactデフォルトのCSSを当てる

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { InlineStyle } from "./components/InlineStyle.jsx";
 
 function App() {
-  return null
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
 }
 
 export default App
---
+++ frontend2/src/components/InlineStyle.jsx
@@ -0,0 +1,28 @@
+export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: '20px',
+    padding: '8px',
+    margin: '8px',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  };
+  const titleStyle = {
+    margin: 0,
+    color: '#3d84a8',
+  };
+  const buttonStyle = {
+    backgroundColor: '#abedd8',
+    border: 'none',
+    padding: '8px',
+    borderRadius: '8px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}>- Inline Styles -</p>
+      <button style={buttonStyle}>Fight!!</button>
+    </div>
+  );
+}
