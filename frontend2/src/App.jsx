bc90b4da8f101b7e85ba5a19d11431615cf036de
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-26 17:56:22 +0900

    CSS Moduleを試す

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { InlineStyle } from "./components/InlineStyle.jsx";
+import { CssModules } from "./components/CssModules.jsx";
 
 function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
--- frontend2/vite.config.js
+++ frontend2/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import sassPlugin from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  plugins: [react()],
+  plugins: [react(), sassPlugin()],
 })
