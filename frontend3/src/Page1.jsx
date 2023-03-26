3a65fcf483dc0584d4c40823346cffd04984469b
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-27 00:47:06 +0900

    表示するページファイルを作成

---
+++ frontend3/src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1です</h1>
+    </div>
+  );
+}
---
+++ frontend3/src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2です</h1>
+    </div>
+  );
+}
