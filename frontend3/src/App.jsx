0ce4f908dcdc8a835d741273b4139aeb3b7e7b98
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-03-29 09:12:46 +0900

    react-router-domを5.x系でインストールし、基本的なルーティングを実現

--- frontend3/src/App.jsx
+++ frontend3/src/App.jsx
@@ -1,9 +1,34 @@
-import { useState } from 'react'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 import './App.css'
 
 function App() {
-
-  return null
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/page1'>Page1</Link>
+        <br />
+        <Link to='/page2'>Page2</Link>
+      </div>
+      <Switch>
+        {/* Linkで定義したpathに一致するものをここで定義。一致したらそのコンポーネントを返す */}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/page1">
+          <Page1 />
+        </Route>
+        <Route exact path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App
