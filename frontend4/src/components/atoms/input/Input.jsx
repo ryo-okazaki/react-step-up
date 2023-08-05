af605ecea497447efdec1492a42a7331d18dc412
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 19:56:26 +0900

    input要素をatom化

---
+++ frontend4/src/components/atoms/input/Input.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Input = (props) => {
+  const {placeholder = ''} = props;
+
+  return <SInput type='text' placeholder={placeholder} />
+}
+
+const SInput = styled.input`
+  padding: 8px 16px;
+  border: solid #ddd 1px;
+  border-radius: 9999px;
+  outline: none;
+`
--- frontend4/src/components/molecules/SearchInput.jsx
+++ frontend4/src/components/molecules/SearchInput.jsx
@@ -1,9 +1,10 @@
 import {PrimaryButton} from "../atoms/buttons/PrimaryButton.jsx";
+import {Input} from "../atoms/input/Input.jsx";
 
 export const SearchInput = () => {
   return (
     <div>
-      <input placeholder='検索条件を入力' />
+      <Input placeholder='検索条件を入力' />
       <PrimaryButton>検索</PrimaryButton>
     </div>
   )
