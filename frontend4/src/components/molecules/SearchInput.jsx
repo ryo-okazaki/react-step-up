1b62a491fddd7d7a4299123c23cfcc4809bea9d1
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 19:59:33 +0900

    CSSで見た目を修正

--- frontend4/src/components/molecules/SearchInput.jsx
+++ frontend4/src/components/molecules/SearchInput.jsx
@@ -1,11 +1,24 @@
 import {PrimaryButton} from "../atoms/buttons/PrimaryButton.jsx";
 import {Input} from "../atoms/input/Input.jsx";
+import styled from "styled-components";
 
 export const SearchInput = () => {
   return (
     <div>
-      <Input placeholder='検索条件を入力' />
-      <PrimaryButton>検索</PrimaryButton>
+      <SContainer>
+        <Input placeholder='検索条件を入力' />
+        <SButtonWrapper>
+          <PrimaryButton>検索</PrimaryButton>
+        </SButtonWrapper>
+      </SContainer>
     </div>
   )
 }
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`
