4617c7c91a972cdfea71689975fa557ed2a9f04e
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 01:04:04 +0900

    子コンポーネントにもisAdminをグローバルステートで渡す

--- frontend4/src/components/molecules/user/UserIconWithName.jsx
+++ frontend4/src/components/molecules/user/UserIconWithName.jsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
+import {useContext} from "react";
+import {UserContext} from "../../../providers/UserProvider.jsx";
 
 export const UserIconWithName = (props) => {
   const {image, name} = props;
+  const {userInfo} = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
 
   return (
     <SContainer>
       <SImage height={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   )
 }
@@ -22,4 +27,9 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `
