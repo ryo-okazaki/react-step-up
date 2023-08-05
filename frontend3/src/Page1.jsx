e21dfa0a562622d17a51bc4741b6329c6b5c47c0
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 15:57:50 +0900

    Linkを使わないページ遷移

--- frontend3/src/Page1.jsx
+++ frontend3/src/Page1.jsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()]
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/page1/detailA");
   return (
     <div>
       <h1>Page1です</h1>
       <Link to={{ pathname: '/page1/detailA', state: arr}}>DetailA</Link>
       <br />
       <Link to='/page1/detailB'>DetailB</Link>
+      <br/>
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 }
--- frontend3/src/Page1DetailA.jsx
+++ frontend3/src/Page1DetailA.jsx
@@ -3,6 +3,7 @@ import {useLocation} from "react-router-dom";
 export const Page1DetailA = () => {
   const {state} = useLocation();
   console.log(state);
+  // ボタンクリック(JSでの遷移)の場合、stateは渡していない
   return (
     <div>
       <h1>Page1DetailAです</h1>
