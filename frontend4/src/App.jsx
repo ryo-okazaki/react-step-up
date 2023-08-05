178846555e55aa348ef897cc2f453b0b00d797c8
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 22:06:21 +0900

    ユーザーページにCSSを適用し、ダミーデータを作成

--- frontend4/src/App.jsx
+++ frontend4/src/App.jsx
@@ -1,17 +1,6 @@
 import './App.css'
 import {Router} from "./router/Router.jsx";
 
-const user = {
-  name: 'test',
-  image: 'https://source.unsplash.com/ZCHj_2lJP00',
-  email: 'test@example.com',
-  phone: '090-1111-2222',
-  company: {
-    name: 'test株式会社',
-  },
-  website: 'https://google.com'
-}
-
 function App() {
   return (
     <Router />
--- frontend4/src/components/pages/Users.jsx
+++ frontend4/src/components/pages/Users.jsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
+import {SearchInput} from "../molecules/SearchInput.jsx";
+
+const users = [...Array(10).keys()].map(((val) => {
+  return {
+    id: val,
+    name: `user-${val}`,
+    image: 'https://source.unsplash.com/ZCHj_2lJP00',
+    email: 'test@example.com',
+    phone: '090-1111-2222',
+    company: {
+      name: 'test株式会社',
+    },
+    website: 'https://google.com'
+  }
+}))
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
+      <SearchInput />
     </SContainer>
   )
 }
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `
