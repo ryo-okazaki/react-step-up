8292fec76d5b1bbae9d0892c8073bc4175cd924c
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 21:52:18 +0900

    TopとUserのコンポーネントを作成

---
+++ frontend4/src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>TOPページ</h2>
+    </SContainer>
+  )
+}
+
+const SContainer = styled.div`
+  text-align: center;
+`
