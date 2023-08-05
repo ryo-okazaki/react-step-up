a13dc94c024702a59604e935bc5472430e36e51d
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 19:33:45 +0900

    ボタンの共通CSSを作成

--- frontend4/src/components/atoms/buttons/PrimaryButton.jsx
+++ frontend4/src/components/atoms/buttons/PrimaryButton.jsx
@@ -1,22 +1,13 @@
 import styled from "styled-components";
+import {BaseButton} from "./BaseButton.jsx";
 
 export const PrimaryButton = (props) => {
   const { children } = props;
-  console.log(props)
   return (
     <SButton>{children}</SButton>
   )
 }
 
-const SButton = styled.button`
+const SButton = styled(BaseButton)`
   background-color: #40514e;
-  color: #fff;
-  padding: 6px 24px;
-  border-radius: 9999px;
-  border: none;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.5;
-  }
 `;
--- frontend4/src/components/atoms/buttons/SecondaryButton.jsx
+++ frontend4/src/components/atoms/buttons/SecondaryButton.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {BaseButton} from "./BaseButton.jsx";
 
 export const SecondaryButton = (props) => {
   const { children } = props;
@@ -8,15 +9,6 @@ export const SecondaryButton = (props) => {
   )
 }
 
-const SButton = styled.button`
+const SButton = styled(BaseButton)`
   background-color: #11999e;
-  color: #fff;
-  padding: 6px 24px;
-  border-radius: 9999px;
-  border: none;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.5;
-  }
 `;
