c9bd09644796b7b63afa74981b6cfea77d690097
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-05 15:28:10 +0900

    stateを渡すページ遷移

--- frontend3/src/Page1.jsx
+++ frontend3/src/Page1.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()]
   return (
     <div>
       <h1>Page1です</h1>
-      <Link to='/page1/detailA'>DetailA</Link>
+      <Link to={{ pathname: '/page1/detailA', state: arr}}>DetailA</Link>
       <br />
       <Link to='/page1/detailB'>DetailB</Link>
     </div>
--- frontend3/src/Page1DetailA.jsx
+++ frontend3/src/Page1DetailA.jsx
@@ -1,4 +1,8 @@
+import {useLocation} from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const {state} = useLocation();
+  console.log(state);
   return (
     <div>
       <h1>Page1DetailAです</h1>
