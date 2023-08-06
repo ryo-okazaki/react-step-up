f1a6b3673845eaafd8e189ffeb1ca79175f398a1
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 11:59:03 +0900

    jsonPlaceholderを使ってaxiosを使う

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -5,8 +5,22 @@ import { CssModules } from "./components/CssModules.jsx";
 import {StyledJsx} from "./components/StyledJsx.jsx";
 import {StyledComponents} from "./components/StyledComponents.jsx";
 import {Emotion} from "./components/Emotion.jsx";
+import axios from "axios";
 
 function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err))
+  }
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
       <InlineStyle />
@@ -14,6 +28,8 @@ function App() {
       <StyledJsx />
       <StyledComponents />
       <Emotion />
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
