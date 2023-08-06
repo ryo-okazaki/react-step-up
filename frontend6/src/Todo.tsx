df02ad38681b0e012a052f93a9e389a4174634c1
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 20:00:00 +0900

    コンポーネント自体の型定義

--- frontend6/src/Todo.tsx
+++ frontend6/src/Todo.tsx
@@ -1,9 +1,11 @@
 import {TodoType} from "./types/todo";
+import {FC} from "react";
 
-export const Todo = (
-  props: Pick<TodoType, "userId" | "title" | "completed">
-  // props: Omit<TodoType, "id">
-) => {
+// export const Todo = (
+//   props: Pick<TodoType, "userId" | "title" | "completed">
+//   // props: Omit<TodoType, "id">
+// ) => {
+  export const Todo: FC<Pick<TodoType, "userId" | "title" | "completed">> = (props) => {
   const {title, userId, completed = false} = props;
   const completedMark = completed ? "[完]" : "[未]";
 
