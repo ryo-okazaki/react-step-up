7544502a4f23c96fe2764af18bfd291f1901145c
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 15:24:31 +0900

    APIの取得確認

--- frontend6/src/App.tsx
+++ frontend6/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const onClickFetchData = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => console.log(res))
+  }
+
   return (
     <>
+      <button onClick={onClickFetchData}>データ取得</button>
     </>
   )
 }
-
-export default App
