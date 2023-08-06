8390b32b0ea058ea3583e24206ee98b8ed92d689
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 19:34:42 +0900

    型宣言を共通化 Pickで必要なpropsを抽出

--- frontend6/src/App.tsx
+++ frontend6/src/App.tsx
@@ -2,13 +2,7 @@ import './App.css'
 import axios from "axios";
 import {useState} from "react";
 import {Todo} from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import {TodoType} from "./types/todo";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([])
@@ -25,7 +19,7 @@ export default function App() {
     <>
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userId={todo.userId} completed={todo.completed} />
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
       ))}
     </>
   )
--- frontend6/src/Todo.tsx
+++ frontend6/src/Todo.tsx
@@ -1,10 +1,8 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-}
+import {TodoType} from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  props: Pick<TodoType, "userId" | "title" | "completed">
+) => {
   const {title, userId, completed = false} = props;
   const completedMark = completed ? "[完]" : "[未]";
 
