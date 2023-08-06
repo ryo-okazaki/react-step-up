f2bca7ea2795c3b5c5e5326acbc2a0cead96497d
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 17:25:22 +0900

    APIの取得データに型定義

--- frontend6/src/App.tsx
+++ frontend6/src/App.tsx
@@ -3,12 +3,19 @@ import axios from "axios";
 import {useState} from "react";
 import {Todo} from "./Todo";
 
+type TodoType = {
+  "userId": number;
+  "id": number;
+  "title": string;
+  "completed": boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([])
+  const [todos, setTodos] = useState<Array<TodoType>>([])
 
   const onClickFetchData = () => {
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
+      .get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
       .then((res) => {
         setTodos(res.data);
       })
@@ -17,8 +24,8 @@ export default function App() {
   return (
     <>
       <button onClick={onClickFetchData}>データ取得</button>
-      {todos.map((todo: any) => (
-        <Todo title={todo.title} userid={todo.userid} />
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </>
   )
