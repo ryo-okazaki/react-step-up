0b96a6bdb77f84391d329503af0fdc50876800ec
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 15:30:17 +0900

    APIの取得確認

--- frontend6/src/App.tsx
+++ frontend6/src/App.tsx
@@ -1,16 +1,24 @@
 import './App.css'
 import axios from "axios";
+import {useState} from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([])
+
   const onClickFetchData = () => {
     axios
       .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => console.log(res))
+      .then((res) => {
+        setTodos(res.data);
+      })
   }
 
   return (
     <>
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo: any) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
     </>
   )
 }
