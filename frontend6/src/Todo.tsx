8e09efa38517d93920ba718ac36911c95ff1ff36
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 19:36:00 +0900

    Omitで不要なpropsを排除

--- frontend6/src/Todo.tsx
+++ frontend6/src/Todo.tsx
@@ -2,6 +2,7 @@ import {TodoType} from "./types/todo";
 
 export const Todo = (
   props: Pick<TodoType, "userId" | "title" | "completed">
+  // props: Omit<TodoType, "id">
 ) => {
   const {title, userId, completed = false} = props;
   const completedMark = completed ? "[完]" : "[未]";
