d9fa53c5d523207b295b0b58d803fae6ad292b16
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-06 14:17:06 +0900

    TypeScriptのメソッドの引数の型指定

--- frontend5/src/App.tsx
+++ frontend5/src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import {Practice1} from "./practice/Practice1";
 
 function App() {
   return (
     <>
+      <Practice1 />
     </>
   )
 }
---
+++ frontend5/src/practice/Practice1.tsx
@@ -0,0 +1,17 @@
+export const Practice1 = () => {
+    const calcTotalFee = (num: number) => {
+        const total = num * 1.1;
+        console.log(total);
+    }
+
+    const onClickPractice = () => {
+        calcTotalFee(1000);
+    }
+
+    return (
+        <div>
+            <p>練習問題：引数の型指定</p>
+            <button onClick={onClickPractice}>練習問題2を実行</button>
+        </div>
+    )
+}
