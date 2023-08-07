6c2b654688c1c2d97deaab89a1dbc71093d39d55
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-08-07 22:54:05 +0900

    カスタムフックを使用しない場合のエラーハンドリング

--- frontend7/src/App.tsx
+++ frontend7/src/App.tsx
@@ -7,8 +7,13 @@ import {UserProfile} from "./types/UserProfile";
 
 export default function App() {
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const fetchUsers = () => {
+    setLoading(true);
+    setError(false);
+
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -17,18 +22,34 @@ export default function App() {
           name: `${user.name}(${user.username})`,
           email: user.email,
           address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
+        }))
 
         setUserProfiles(data);
       })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
 
   return (
     <>
       <button onClick={fetchUsers}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <br/>
+      {error ? (
+        <p style={{color: "red"}}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+          <>
+            {userProfiles.map((user) => (
+              <UserCard key={user.id} user={user} />
+            ))}
+          </>
+        )
+      }
     </>
   )
 }
